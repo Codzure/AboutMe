ef2e18d3c1a894a07a52619198d36c0b1cf50ba0
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2023-01-10 14:15:56 +0300

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Leonard Mutugi on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
